98c5aeeabe79b7b90434453eef5351e12b5ddaa3
Aroon Gurung <arun2gurung.gmail.com>
2024-11-19 18:17:56 +0200

    new changes

---
+++ app/page.tsx
@@ -0,0 +1,14 @@
+import Hero from "@/components/Hero";
+import Products from "@/components/Products";
+import ReviewInfo from "@/components/ReviewInfo";
+
+
+export default function Home() {
+  return (
+    <>
+      <Hero />
+      <Products />
+      <ReviewInfo />  
+    </>
+  );
+}
